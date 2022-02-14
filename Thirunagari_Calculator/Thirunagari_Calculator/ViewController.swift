943fbba05c32054c1dcde086a588b4b53296d2d3
vamshi krishna <vamshikrishna3095@gmail.com>
2022-02-14 14:44:15 -0600

    CHANGES

---
+++ Thirunagari_Calculator/Thirunagari_Calculator/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  Thirunagari_Calculator
+//
+//  Created by Thirunagari,Vamshi Krishna on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var LabelResult: UILabel!
+    
+    
+    @IBAction func ACButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func CButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func plus_minButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func DivisionButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func ZeroButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func OneButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func TwoButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func ThreeButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func FourButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func FiveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func SixButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func SevenButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func EightButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func NineButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func DotButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func PercentButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func MultiplicationButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func MinusButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func PlusButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func EqualsToButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
+
